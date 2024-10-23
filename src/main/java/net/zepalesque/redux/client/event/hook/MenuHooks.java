45a73ac215b434c1ffd22db78f4a907cbadc2867
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-10-22 23:23:51 -0400

    fix: fixes

--- src/main/java/net/zepalesque/redux/client/event/hook/MenuHooks.java
+++ src/main/java/net/zepalesque/redux/client/event/hook/MenuHooks.java
@@ -5,10 +5,6 @@ import net.zepalesque.redux.client.ReduxMenus;
 
 public class MenuHooks {
     public static void prepareCustomMenus(MenuHelper menuHelper) {
-        menuHelper.prepareMenu(ReduxMenus.BLIGHT_MENU.get());
-        menuHelper.prepareMenu(ReduxMenus.GILDED_MENU.get());
-        menuHelper.prepareMenu(ReduxMenus.DUNGEON_MENU.get());
-        menuHelper.prepareMenu(ReduxMenus.CLOUDCAPS_MENU.get());
-        menuHelper.prepareMenu(ReduxMenus.SKYFIELDS_MENU.get());
+        menuHelper.prepareMenu(ReduxMenus.REDUX_MENU.get());
     }
 }
