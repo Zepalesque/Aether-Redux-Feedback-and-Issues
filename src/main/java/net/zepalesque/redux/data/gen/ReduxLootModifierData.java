ca92a6961acc24958562b54cff519345cff75172
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-12-09 19:35:29 -0500

    feat: add loot modifier

--- src/main/java/net/zepalesque/redux/data/gen/ReduxLootModifierData.java
+++ src/main/java/net/zepalesque/redux/data/gen/ReduxLootModifierData.java
@@ -1,8 +1,12 @@
 package net.zepalesque.redux.data.gen;
 
+import com.aetherteam.aether.loot.AetherLoot;
+import com.aetherteam.nitrogen.loot.modifiers.AddDungeonLootModifier;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.util.random.WeightedEntry;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
@@ -11,7 +15,9 @@ import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.neoforged.neoforge.common.loot.LootTableIdCondition;
 import net.zepalesque.redux.Redux;
 import net.zepalesque.redux.block.ReduxBlocks;
 import net.zepalesque.redux.config.ReduxConfig;
@@ -21,6 +27,7 @@ import net.zepalesque.redux.loot.modifer.RawOreModifier;
 import net.zepalesque.zenith.api.condition.ConfigCondition;
 import net.zepalesque.zenith.loot.condition.ConditionLootModule;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ReduxLootModifierData extends ReduxLootModifierProvider {
@@ -42,5 +49,18 @@ public class ReduxLootModifierData extends ReduxLootModifierProvider {
                         hasSilkTouch().invert().build(),
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ReduxBlocks.VERIDIUM_ORE.get()).build()}));
 
+        this.add("aerbound_cape", new AddDungeonLootModifier(
+                new LootItemCondition[] {
+                        LootTableIdCondition.builder(AetherLoot.BRONZE_DUNGEON_REWARD.location())/*.or(LootTableIdCondition.builder(AncientAetherLoot.CHESTS_DUNGEON_BRONZE_DUNGEON_REWARD))*/.build(),
+                        LootItemRandomChanceCondition.randomChance(0.75F).build()
+                },
+                List.of(
+                        WeightedEntry.wrap(new ItemStack(ReduxItems.AERBOUND_CAPE.get()), 1)
+                ),
+                ConstantInt.of(1)
+        ));
+
     }
+
+
 }
