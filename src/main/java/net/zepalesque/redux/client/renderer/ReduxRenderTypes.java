f675594e7db2e753ff868e89862f7de536ab9b20
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-12-23 19:31:18 -0500

    feat: Maybe fix stuff? i hope lmao

---
+++ src/main/java/net/zepalesque/redux/client/renderer/ReduxRenderTypes.java
@@ -0,0 +1,30 @@
+package net.zepalesque.redux.client.renderer;
+
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+
+public class ReduxRenderTypes {
+    public static RenderType whirlwind(ResourceLocation location, float u, float v) {
+        return RenderType.create(
+                "whirlwind",
+                DefaultVertexFormat.NEW_ENTITY,
+                VertexFormat.Mode.QUADS,
+                1536,
+                false,
+                true,
+                RenderType.CompositeState.builder()
+                        .setShaderState(RenderType.RENDERTYPE_BREEZE_WIND_SHADER)
+                        .setTextureState(new RenderStateShard.TextureStateShard(location, false, false))
+                        .setTexturingState(new RenderStateShard.OffsetTexturingStateShard(u, v))
+                        .setTransparencyState(RenderType.TRANSLUCENT_TRANSPARENCY)
+                        .setCullState(RenderType.NO_CULL)
+                        .setLightmapState(RenderType.LIGHTMAP)
+                        .setOverlayState(RenderType.NO_OVERLAY)
+                        .setOutputState(RenderType.TRANSLUCENT_TARGET)
+                        .createCompositeState(false)
+        );
+    }
+}
--- src/main/java/net/zepalesque/redux/client/renderer/entity/ReduxWhirlwindRenderer.java
+++ src/main/java/net/zepalesque/redux/client/renderer/entity/ReduxWhirlwindRenderer.java
@@ -1,29 +1,18 @@
 package net.zepalesque.redux.client.renderer.entity;
 
 import com.aetherteam.aether.Aether;
-import com.aetherteam.aether.client.renderer.AetherModelLayers;
-import com.aetherteam.aether.client.renderer.entity.model.AechorPlantModel;
 import com.aetherteam.aether.entity.AetherEntityTypes;
 import com.aetherteam.aether.entity.monster.AbstractWhirlwind;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.QuadrupedModel;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.BreezeRenderer;
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.SquidRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.animal.Squid;
-import net.minecraft.world.inventory.InventoryMenu;
-import net.zepalesque.redux.Redux;
+import net.zepalesque.redux.client.renderer.ReduxRenderTypes;
 import net.zepalesque.redux.client.renderer.ReduxRenderers;
 import net.zepalesque.redux.client.renderer.entity.model.WhirlwindModel;
 import net.zepalesque.redux.config.ReduxConfig;
@@ -48,19 +37,23 @@ public class ReduxWhirlwindRenderer<T extends AbstractWhirlwind> extends LivingE
     @Override
     public void render(@NotNull T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
         if (ReduxConfig.CLIENT.improved_whirlwinds.get()) {
+
+            boolean isEvil = entity.getType() == AetherEntityTypes.EVIL_WHIRLWIND.get();
             float age = this.getBob(entity, partialTicks);
-            VertexConsumer vertexconsumer = buffer.getBuffer(RenderType.breezeWind(getTextureLocation(entity), this.xOffset(age) % 1.0F, 0.0F));
+            VertexConsumer vertexconsumer = buffer.getBuffer(ReduxRenderTypes.whirlwind(getTextureLocation(entity), this.xOffset(age, isEvil) % 1.0F, 0.0F));
             poseStack.pushPose();
             this.model.setupAnim(entity, 0.0F, 0.0F, age, 0.0F, 0.0F);
             poseStack.mulPose(Axis.ZP.rotationDegrees(180.0F));
-            if (entity.getType() == AetherEntityTypes.EVIL_WHIRLWIND.get()) poseStack.scale(1.25F, 1.25F, 1.25F);
+
+            if (isEvil) poseStack.scale(1.25F, 1.25F, 1.25F);
             this.model.renderToBuffer(poseStack, vertexconsumer, packedLight, OverlayTexture.NO_OVERLAY);
             poseStack.popPose();
         }
     }
 
-    private float xOffset(float tickCount) {
-        return tickCount * 0.01F;
+
+    private float xOffset(float tickCount, boolean isEvil) {
+        return tickCount * (isEvil ? 0.015F : 0.01F);
     }
 
     @Override
