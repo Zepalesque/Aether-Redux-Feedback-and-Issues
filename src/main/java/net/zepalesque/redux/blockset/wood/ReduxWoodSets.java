fece53dedf68a21a9cbb41f018aeedfa05fac7fc
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-01-01 17:39:34 -0500

    feat: Blightwillow woodset in a single line lmao

--- src/main/java/net/zepalesque/redux/blockset/wood/ReduxWoodSets.java
+++ src/main/java/net/zepalesque/redux/blockset/wood/ReduxWoodSets.java
@@ -10,6 +10,7 @@ import net.zepalesque.redux.blockset.wood.type.RegularBookshelfSet;
 public class ReduxWoodSets {
 
     public static final LogWallWoodSet CRYSTAL = register(new RegularBookshelfSet("crystal", MapColor.COLOR_LIGHT_BLUE, MapColor.TERRACOTTA_CYAN, SoundType.WOOD));
+    public static final LogWallWoodSet BLIGHTWILLOW = register(new RegularBookshelfSet("blightwillow", MapColor.COLOR_GREEN, MapColor.COLOR_GRAY, SoundType.CHERRY_WOOD));
 
     public static <T extends AbstractBookshelfSet<?>> T register(T set) {
         Redux.BLOCK_SETS.add(set);
