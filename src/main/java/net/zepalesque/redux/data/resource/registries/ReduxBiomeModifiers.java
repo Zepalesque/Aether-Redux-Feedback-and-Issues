ba50c0e1023fa4542cb7ebe903e26a7746b3cb0f
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-01-11 11:28:15 -0500

    improv: darken blight sky

--- src/main/java/net/zepalesque/redux/data/resource/registries/ReduxBiomeModifiers.java
+++ src/main/java/net/zepalesque/redux/data/resource/registries/ReduxBiomeModifiers.java
@@ -55,11 +55,11 @@ public class ReduxBiomeModifiers {
                 Optional.of(new SkiesModifier.DefaultSkySettings(biomes.getOrThrow(ReduxTags.Biomes.MODIFY_SKY_COLOR), Optional.of(0x9FA4DD), Optional.of(0xBEC4E5))),
                 ImmutableMap.<Holder<Biome>, Integer>builder() // sky
                         .put(biomes.getOrThrow(ReduxBiomes.GILDED_GROVES), 0xC4BDAA)
-                        .put(biomes.getOrThrow(ReduxBiomes.THE_BLIGHT), 0x8F90BA)
+                        .put(biomes.getOrThrow(ReduxBiomes.THE_BLIGHT), 0x7C7DA5)
                         .build(),
                 ImmutableMap.<Holder<Biome>, Integer>builder() // fog
                         .put(biomes.getOrThrow(ReduxBiomes.GILDED_GROVES), 0xDDD9DA)
-                        .put(biomes.getOrThrow(ReduxBiomes.THE_BLIGHT), 0xBCBDE2)
+                        .put(biomes.getOrThrow(ReduxBiomes.THE_BLIGHT), 0xADAED3)
                         .build());
         context.register(SKY_COLOR_AETHER, new ConditionalBiomeModifier(Holder.direct(sky), conditions.get(ReduxConditions.SKY_COLORS).orElseThrow()));
 
