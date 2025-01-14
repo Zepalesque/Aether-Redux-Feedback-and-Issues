d1a0b802d0658db18f69275589453d2dede93752
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2025-01-14 17:03:34 -0500

    feat: recolor bleakmoss further

--- src/main/java/net/zepalesque/redux/client/ReduxColors.java
+++ src/main/java/net/zepalesque/redux/client/ReduxColors.java
@@ -22,7 +22,7 @@ public class ReduxColors {
         public static final int GILDED_GRASS_COLOR = 0xF3FFDD;
         public static final int BLIGHT_GRASS_COLOR = 0xBEAEE5;
 
-        public static final int BLEAKMOSS_GRASS_COLOR = 0xC5A2D6;
+        public static final int BLEAKMOSS_GRASS_COLOR = 0xB79EC1;
     }
 
     public static Optional<Integer> reduxColors(BlockState state, BlockAndTintGetter level, BlockPos pos, int index, Predicate<Integer> indexGoal, boolean useBelowProperties) {
